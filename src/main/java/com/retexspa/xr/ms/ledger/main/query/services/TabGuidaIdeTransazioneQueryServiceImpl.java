ea3eecf7237b5d12eb691cbfa8fb137612d5eafe
Manel Bousabbat <manelbousabbat@msstn.net>
2024-06-19 16:22:17 +0100

    feat: IXL-85-TAB_GUIDA_IDE_TRANSAZIONE

--- src/main/java/com/retexspa/xr/ms/ledger/main/query/services/TabGuidaIdeTransazioneQueryServiceImpl.java
+++ src/main/java/com/retexspa/xr/ms/ledger/main/query/services/TabGuidaIdeTransazioneQueryServiceImpl.java
@@ -44,114 +44,153 @@ public class TabGuidaIdeTransazioneQueryServiceImpl implements  TabGuidaIdeTrans
                         break;
 
                     case "lista_01":
+                        baseSort.setOrderBy("lista01");
                         break;
 
-                    case "padDir_01":
+                    case "pad_dir_01":
+                        baseSort.setOrderBy("padDir01");
                         break;
 
-                    case "padChr_01":
+                    case "pad_chr_01":
+                        baseSort.setOrderBy("padChr01");
                         break;
 
-                    case "posField_01":
+                    case "pos_field_01":
+                        baseSort.setOrderBy("posField01");
                         break;
 
-                    case "lenField_01":
+                    case "len_field_01":
+                        baseSort.setOrderBy("lenField01");
                         break;
-                    case "flgEan_01":
+                    case "flg_ean_01":
+                        baseSort.setOrderBy("flgEan01");
                         break;
-                    case "tipoField_01":
-                        break;
-
-                    case "ctrlField_01":
-                        break;
-
-                    case "suggField_01":
+                    case "tipo_field_01":
+                        baseSort.setOrderBy("tipoField01");
                         break;
 
+                    case "ctrl_field_01":
+                        baseSort.setOrderBy("ctrlField01");
+                        break;
+
+                    case "sugg_field_01":
+                        baseSort.setOrderBy("suggField01");
+                        break;
                     case "lista_02":
+                        baseSort.setOrderBy("lista02");
                         break;
 
-                    case "padDir_02":
+                    case "pad_dir_02":
+                        baseSort.setOrderBy("padDir02");
                         break;
 
-                    case "padChr_02":
+                    case "pad_chr_02":
+                        baseSort.setOrderBy("padChr02");
                         break;
 
-                    case "posField_02":
+                    case "pos_field_02":
+                        baseSort.setOrderBy("posField02");
                         break;
 
-                    case "lenField_02":
+                    case "len_field_02":
+                        baseSort.setOrderBy("lenField02");
                         break;
-                    case "flgEan_02":
+                    case "flg_ean_02":
+                        baseSort.setOrderBy("flgEan02");
                         break;
-                    case "tipoField_02":
-                        break;
-
-                    case "ctrlField_02":
-                        break;
-
-                    case "suggField_02":
+                    case "tipo_field_02":
+                        baseSort.setOrderBy("tipoField02");
                         break;
 
+                    case "ctrl_field_02":
+                        baseSort.setOrderBy("ctrlField02");
+                        break;
+
+                    case "sugg_field_02":
+                        baseSort.setOrderBy("suggField02");
+                        break;
                     case "lista_03":
+                        baseSort.setOrderBy("lista03");
                         break;
 
-                    case "padDir_03":
+                    case "pad_dir_03":
+                        baseSort.setOrderBy("padDir03");
                         break;
 
-                    case "padChr_03":
+                    case "pad_chr_03":
+                        baseSort.setOrderBy("padChr03");
                         break;
 
-                    case "posField_03":
+                    case "pos_field_03":
+                        baseSort.setOrderBy("posField03");
                         break;
 
-                    case "lenField_03":
+                    case "len_field_03":
+                        baseSort.setOrderBy("lenField03");
                         break;
-                    case "flgEan_03":
+                    case "flg_ean_03":
+                        baseSort.setOrderBy("flgEan03");
                         break;
-                    case "tipoField_03":
+                    case "tipo_field_03":
+                        baseSort.setOrderBy("tipoField03");
                         break;
 
-                    case "ctrlField_03":
+                    case "ctrl_field_03":
+                        baseSort.setOrderBy("ctrlField03");
                         break;
 
-                    case "suggField_03":
+                    case "sugg_field_03":
+                        baseSort.setOrderBy("suggField03");
                         break;
 
                     case "lista_04":
+                        baseSort.setOrderBy("lista04");
                         break;
 
-                    case "padDir_04":
+                    case "pad_dir_04":
+                        baseSort.setOrderBy("padDir04");
                         break;
 
-                    case "padChr_04":
+                    case "pad_chr_04":
+                        baseSort.setOrderBy("padChr04");
                         break;
 
-                    case "posField_04":
+                    case "pos_field_04":
+                        baseSort.setOrderBy("posField04");
                         break;
 
-                    case "lenField_04":
+                    case "len_field_04":
+                        baseSort.setOrderBy("lenField04");
                         break;
-                    case "flgEan_04":
+                    case "flg_ean_04":
+                        baseSort.setOrderBy("flgEan04");
                         break;
-                    case "tipoField_04":
-                        break;
-
-                    case "ctrlField_04":
-                        break;
-
-                    case "suggField_04":
+                    case "tipo_field_04":
+                        baseSort.setOrderBy("tipoField04");
                         break;
 
-                    case "flgEanAltro":
+                    case "ctrl_field_04":
+                        baseSort.setOrderBy("ctrlField04");
                         break;
-                    case "descAltro":
+
+                    case "sugg_field_04":
+                        baseSort.setOrderBy("suggField04");
                         break;
-                    case "lenEan":
+
+                    case "flg_ean_altro":
+                        baseSort.setOrderBy("flgEanAltro");
                         break;
-                    case "flgCancellato":
+                    case "desc_altro":
+                        baseSort.setOrderBy("descAltro");
                         break;
-                    case "dataCancellazione" :
+                    case "len_ean":
+                        baseSort.setOrderBy("lenEan");
+                        break;
+                    case "flg_cancellato":
+                        baseSort.setOrderBy("flgCancellato");
+                        break;
+                    case "data_cancellazione" :
+                        baseSort.setOrderBy("dataCancellazione");
                         break;
                     case "version":
                         break;
@@ -187,178 +226,178 @@ public class TabGuidaIdeTransazioneQueryServiceImpl implements  TabGuidaIdeTrans
         if (filter.getLista_01() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("lista_01")), "%" + filter.getLista_01().toUpperCase() + "%"));
+                            c.upper(r.get("lista01")), "%" + filter.getLista_01().toUpperCase() + "%"));
         }
         if (filter.getPadDir_01() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("padDir_01")), "%" + filter.getPadDir_01().toUpperCase() + "%"));
+                            c.upper(r.get("padDir01")), "%" + filter.getPadDir_01().toUpperCase() + "%"));
         }
         if (filter.getPadChr_01() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("padChr_01")), "%" + filter.getPadChr_01().toUpperCase() + "%"));
+                            c.upper(r.get("padChr01")), "%" + filter.getPadChr_01().toUpperCase() + "%"));
         }
 
         if (filter.getPosField_01() != null) {
             specifications.add(
-                    (r, q, c) -> c.equal(r.get("posField_01"),  filter.getPosField_01()));
+                    (r, q, c) -> c.equal(r.get("posField01"),  filter.getPosField_01()));
         }
         if (filter.getLenField_01() != null) {
             specifications.add(
-                    (r, q, c) -> c.equal(r.get("lenField_01"),  filter.getLenField_01()));
+                    (r, q, c) -> c.equal(r.get("lenField01"),  filter.getLenField_01()));
         }
         if (filter.getFlgEan_01() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("flgEan_01")), "%" + filter.getFlgEan_01().toUpperCase() + "%"));
+                            c.upper(r.get("flgEan01")), "%" + filter.getFlgEan_01().toUpperCase() + "%"));
         }
         if (filter.getTipoField_01() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("tipoField_01")), "%" + filter.getTipoField_01().toUpperCase() + "%"));
+                            c.upper(r.get("tipoField01")), "%" + filter.getTipoField_01().toUpperCase() + "%"));
         }
         if (filter.getCtrlField_01() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("ctrlField_01")), "%" + filter.getCtrlField_01().toUpperCase() + "%"));
+                            c.upper(r.get("ctrlField01")), "%" + filter.getCtrlField_01().toUpperCase() + "%"));
         }
         if (filter.getSuggField_01() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("suggField_01")), "%" + filter.getSuggField_01().toUpperCase() + "%"));
+                            c.upper(r.get("suggField01")), "%" + filter.getSuggField_01().toUpperCase() + "%"));
         }
         if (filter.getLista_02() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("lista_02")), "%" + filter.getLista_02().toUpperCase() + "%"));
+                            c.upper(r.get("lista02")), "%" + filter.getLista_02().toUpperCase() + "%"));
         }
         if (filter.getPadDir_02() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("padDir_02")), "%" + filter.getPadDir_02().toUpperCase() + "%"));
+                            c.upper(r.get("padDir02")), "%" + filter.getPadDir_02().toUpperCase() + "%"));
         }
         if (filter.getPadChr_02() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("padChr_02")), "%" + filter.getPadChr_02().toUpperCase() + "%"));
+                            c.upper(r.get("padChr02")), "%" + filter.getPadChr_02().toUpperCase() + "%"));
         }
 
         if (filter.getPosField_02() != null) {
             specifications.add(
-                    (r, q, c) -> c.equal(r.get("posField_02"),  filter.getPosField_02()));
+                    (r, q, c) -> c.equal(r.get("posField02"),  filter.getPosField_02()));
         }
         if (filter.getLenField_02() != null) {
             specifications.add(
-                    (r, q, c) -> c.equal(r.get("lenField_02"),  filter.getLenField_02()));
+                    (r, q, c) -> c.equal(r.get("lenField02"),  filter.getLenField_02()));
         }
         if (filter.getFlgEan_02() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("flgEan_02")), "%" + filter.getFlgEan_02().toUpperCase() + "%"));
+                            c.upper(r.get("flgEan02")), "%" + filter.getFlgEan_02().toUpperCase() + "%"));
         }
         if (filter.getTipoField_02() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("tipoField_02")), "%" + filter.getTipoField_02().toUpperCase() + "%"));
+                            c.upper(r.get("tipoField02")), "%" + filter.getTipoField_02().toUpperCase() + "%"));
         }
         if (filter.getCtrlField_02() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("ctrlField_02")), "%" + filter.getCtrlField_02().toUpperCase() + "%"));
+                            c.upper(r.get("ctrlField02")), "%" + filter.getCtrlField_02().toUpperCase() + "%"));
         }
         if (filter.getSuggField_02() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("suggField_02")), "%" + filter.getSuggField_02().toUpperCase() + "%"));
+                            c.upper(r.get("suggField02")), "%" + filter.getSuggField_02().toUpperCase() + "%"));
         }
         if (filter.getLista_03() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("lista_03")), "%" + filter.getLista_03().toUpperCase() + "%"));
+                            c.upper(r.get("lista03")), "%" + filter.getLista_03().toUpperCase() + "%"));
         }
         if (filter.getPadDir_03() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("padDir_03")), "%" + filter.getPadDir_03().toUpperCase() + "%"));
+                            c.upper(r.get("padDir03")), "%" + filter.getPadDir_03().toUpperCase() + "%"));
         }
         if (filter.getPadChr_03() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("padChr_03")), "%" + filter.getPadChr_03().toUpperCase() + "%"));
+                            c.upper(r.get("padChr03")), "%" + filter.getPadChr_03().toUpperCase() + "%"));
         }
 
         if (filter.getPosField_03() != null) {
             specifications.add(
-                    (r, q, c) -> c.equal(r.get("posField_03"),  filter.getPosField_03()));
+                    (r, q, c) -> c.equal(r.get("posField03"),  filter.getPosField_03()));
         }
         if (filter.getLenField_03() != null) {
             specifications.add(
-                    (r, q, c) -> c.equal(r.get("lenField_03"),  filter.getLenField_03()));
+                    (r, q, c) -> c.equal(r.get("lenField03"),  filter.getLenField_03()));
         }
         if (filter.getFlgEan_03() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("flgEan_03")), "%" + filter.getFlgEan_03().toUpperCase() + "%"));
+                            c.upper(r.get("flgEan03")), "%" + filter.getFlgEan_03().toUpperCase() + "%"));
         }
         if (filter.getTipoField_03() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("tipoField_03")), "%" + filter.getTipoField_03().toUpperCase() + "%"));
+                            c.upper(r.get("tipoField03")), "%" + filter.getTipoField_03().toUpperCase() + "%"));
         }
         if (filter.getCtrlField_03() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("ctrlField_03")), "%" + filter.getCtrlField_03().toUpperCase() + "%"));
+                            c.upper(r.get("ctrlField03")), "%" + filter.getCtrlField_03().toUpperCase() + "%"));
         }
         if (filter.getSuggField_03() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("suggField_03")), "%" + filter.getSuggField_03().toUpperCase() + "%"));
+                            c.upper(r.get("suggField03")), "%" + filter.getSuggField_03().toUpperCase() + "%"));
         }
         if (filter.getLista_04() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("lista_04")), "%" + filter.getLista_04().toUpperCase() + "%"));
+                            c.upper(r.get("lista04")), "%" + filter.getLista_04().toUpperCase() + "%"));
         }
         if (filter.getPadDir_04() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("padDir_04")), "%" + filter.getPadDir_04().toUpperCase() + "%"));
+                            c.upper(r.get("padDir04")), "%" + filter.getPadDir_04().toUpperCase() + "%"));
         }
         if (filter.getPadChr_04() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("padChr_04")), "%" + filter.getPadChr_04().toUpperCase() + "%"));
+                            c.upper(r.get("padChr04")), "%" + filter.getPadChr_04().toUpperCase() + "%"));
         }
 
         if (filter.getPosField_04() != null) {
             specifications.add(
-                    (r, q, c) -> c.equal(r.get("posField_04"),  filter.getPosField_04()));
+                    (r, q, c) -> c.equal(r.get("posField04"),  filter.getPosField_04()));
         }
         if (filter.getLenField_04() != null) {
             specifications.add(
-                    (r, q, c) -> c.equal(r.get("lenField_04"),  filter.getLenField_04()));
+                    (r, q, c) -> c.equal(r.get("lenField04"),  filter.getLenField_04()));
         }
         if (filter.getFlgEan_04() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("flgEan_04")), "%" + filter.getFlgEan_04().toUpperCase() + "%"));
+                            c.upper(r.get("flgEan04")), "%" + filter.getFlgEan_04().toUpperCase() + "%"));
         }
         if (filter.getTipoField_04() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("tipoField_04")), "%" + filter.getTipoField_04().toUpperCase() + "%"));
+                            c.upper(r.get("tipoField04")), "%" + filter.getTipoField_04().toUpperCase() + "%"));
         }
         if (filter.getCtrlField_04() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("ctrlField_04")), "%" + filter.getCtrlField_04().toUpperCase() + "%"));
+                            c.upper(r.get("ctrlField04")), "%" + filter.getCtrlField_04().toUpperCase() + "%"));
         }
         if (filter.getSuggField_04() != null) {
             specifications.add(
                     (r, q, c) -> c.like(
-                            c.upper(r.get("suggField_04")), "%" + filter.getSuggField_04().toUpperCase() + "%"));
+                            c.upper(r.get("suggField04")), "%" + filter.getSuggField_04().toUpperCase() + "%"));
         }
 
         if (filter.getFlgEanAltro() != null) {
