5b8241f20f81b42fad4f4cbbae4254851a8e2300
Manel Bousabbat <manelbousabbat@msstn.net>
2024-07-29 11:45:08 +0100

    feat: IXL-104-Refactoring-RegRigheChiusureOpe

--- src/main/java/com/retexspa/xr/ms/ledger/main/query/repositories/RegDettChiusureOpeRepository.java
+++ src/main/java/com/retexspa/xr/ms/ledger/main/query/repositories/RegDettChiusureOpeRepository.java
@@ -1,9 +1,13 @@
 package com.retexspa.xr.ms.ledger.main.query.repositories;
 
 import com.retexspa.xr.ms.ledger.main.query.entities.RegDettChiusureOpeQueryEntity;
+import com.retexspa.xr.ms.ledger.main.query.entities.RegRigheChiusureOpeQueryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface RegDettChiusureOpeRepository extends JpaRepository<RegDettChiusureOpeQueryEntity, String>,
         JpaSpecificationExecutor<RegDettChiusureOpeQueryEntity> {
+   List<RegDettChiusureOpeQueryEntity> findAllByRigaChiOpeId(String rigaChiOpeId);
 }
