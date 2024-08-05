0891b824d79c85d77e9f6076f169d8d23f16ac66
jihenmessaabi <jihenmessaabi@msstn.net>
2024-08-05 18:31:40 +0100

    feat: IXL-113 Refactoring-TabTipiPagamento

--- src/main/java/com/retexspa/xr/ms/ledger/main/query/repositories/TabTipiPagamentoAttributoRepository.java
+++ src/main/java/com/retexspa/xr/ms/ledger/main/query/repositories/TabTipiPagamentoAttributoRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TabTipiPagamentoAttributoRepository  extends JpaRepository<TabTipiPagamentoAttributoQueryEntity, String>,
         JpaSpecificationExecutor<TabTipiPagamentoAttributoQueryEntity> {
@@ -13,7 +14,7 @@ public interface TabTipiPagamentoAttributoRepository  extends JpaRepository<TabT
 
     void deleteByTabTipiPagamentoId(String tabTipiPagamentoId);
 
-    List<TabTipiPagamentoAttributoQueryEntity> findByTabTipiPagamentoIdAndAttributoId(
+    Optional<TabTipiPagamentoAttributoQueryEntity> findByTabTipiPagamentoIdAndAttributoId(
             String id, String attributoId);
 
     void deleteByTabTipiPagamentoIdAndAttributoId(String id, String attributoId);
